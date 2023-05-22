38e2c6cec751de7595755302e791b6c58464036e
Olga Prosviriakova <ol7897@ya.ru>
2023-05-22 23:08:58 +0200

    func added

---
+++ ft_memcmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/22 21:13:21 by oprosvir          #+#    #+#             */
+/*   Updated: 2023/05/22 21:13:21 by oprosvir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+    unsigned char	*ptr1;
+    unsigned char	*ptr2;
+
+    ptr1 = (unsigned char *)s1;
+    ptr2 = (unsigned char *)s2;
+    while (n-- > 0)
+    {
+        if (*ptr1 != *ptr2)
+            return (*ptr1 - *ptr2);
+        ptr1++;
+        ptr2++;
+    }
+    return (0);
+}
